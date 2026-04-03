a8bd616a7336a8e983e888e6ad1c1c2a3fc05093
HDSM Jayawardhana <salindamadushansalinda@gmail.com>
2026-04-04 00:32:08 +0530

    Update register_b.v

--- Verilog/register_b.v
+++ Verilog/register_b.v
@@ -1,1 +1,26 @@
+// ============================================================
+// CO221 - Digital Design | SAP-1 Simple Computer
+// Module: Register B
+// Load 8-bit from bus when LB is LOW with CLK
+// Always output to ALU
+// ============================================================
 
+module register_b (
+    input        clk,
+    input        LB,        // Active LOW load
+    input  [7:0] bus_in,
+    output [7:0] alu_out    // Always output to ALU
+);
+
+    reg [7:0] data;
+
+    // Load from bus on rising edge when LB is LOW
+    always @(posedge clk) begin
+        if (!LB)
+            data <= bus_in;
+    end
+
+    // Always output to ALU
+    assign alu_out = data;
+
+endmodule
